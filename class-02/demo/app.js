461a806e3ade36422962b387b09a61898f0ab474
Calvin Cheng <calvincheng919@gmail.com>
2019-09-24 12:53:09 -0700

    class-02 lab and demo

---
+++ class-02/demo/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// alert('This is proof of life');
+
+var userName = prompt('Please give me your name');
+// alert('hello ' + userName + ' how are you doing today');
+
+// var favColor = prompt('What is your favorite color');
+
+// alert(userName + ' I see your favorite color is ' + favColor);
+
+// var favSport = prompt('What is your favorite sport');
+
+// alert('Wow, my favorite sport is ' + favSport + ' as well. It was very nice to meet you, ' + userName);
+
+var loveDogs = prompt('Hey ' + userName + ' do you like dogs?');
+
+
+
+if (loveDogs.toUpperCase() === 'YES' ) {
+  alert('great!');
+} else {
+  alert('oh, that\'s too bad');
+}
+
+
